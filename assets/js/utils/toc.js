a545deb3a05f8f05421a25c81eaf18642cfaa855
hpqc <chinafang1234@gmail.com>
2023-04-24 17:20:01 +0800

    toc固定层模版组件全部开发完成

--- assets/js/utils/toc.js
+++ assets/js/utils/toc.js
@@ -1,25 +1,31 @@
 function handleScroll() {
-    var tocList = document.querySelector("#toc_post_box .toc");
-    var lastToc = tocList.lastElementChild;
-    var lastTocOffset = lastToc.getBoundingClientRect().top + window.scrollY;
-    var windowHeight = window.innerHeight;
+    let tocList = document.querySelector("#toc_post_box .toc");
+    
+    if($(tocList).length === 0){
+        return 
+    }
+    // let lastToc = tocList.lastElementChild;
+    // let lastTocOffset = lastToc.getBoundingClientRect().top + window.scrollY;
+    // let windowHeight = window.innerHeight;
     let postBody = document.querySelector(".post-body");
-    console.log("postBody", postBody);
+    // console.log("postBody", postBody);
     let postBodyOffset = postBody.getBoundingClientRect().top + window.scrollY;
-    console.log("postBodyOffset=", postBodyOffset - 66);
-    console.log("lastTocOffset=", lastTocOffset);
-    console.log("windowHeight=", windowHeight);
-    console.log("window.scrollY=", window.scrollY + 66);
-    console.log("lastToc-距离顶部的距离", lastTocOffset);
-    console.log("lastToc.getBoundingClientRect().top=", lastToc.getBoundingClientRect().top);
+    // console.log("postBodyOffset=", postBodyOffset - 66);
+    // console.log("lastTocOffset=", lastTocOffset);
+    // console.log("windowHeight=", windowHeight);
+    // console.log("window.scrollY=", window.scrollY + 66);
+    // console.log("lastToc-距离顶部的距离", lastTocOffset);
+    // console.log("lastToc.getBoundingClientRect().top=", lastToc.getBoundingClientRect().top);
     let contentOffset = postBodyOffset - 66;
     let scrollChangeSet = window.scrollY + 66;
     if (scrollChangeSet >= contentOffset) {
-        tocList.style.display = "none";
-        // $("#toc_fix_box").show();
+        // $("#toc_post_box").hide();
+        // tocList.style.display = "none";
+        $("#toc_fix_box").show();
     } else {
-        tocList.style.display = "block";
-        // $("#toc_fix_box").hide();
+        // $("#toc_post_box").show();
+        // tocList.style.display = "block";
+        $("#toc_fix_box").hide();
     }
     // if(window.scrollY >= lastTocOffset ){
     //     tocList.style.display = "none";
@@ -31,6 +37,30 @@ function handleScroll() {
     // } else {
     //   tocList.style.display = "none";
     // }
+    $("#toc_fix_box").off('click','.ext_toc_btn')
+    $("#toc_fix_box").on('click','.ext_toc_btn',function(){
+        console.log("hello");
+        let contentBox = $('#toc_fix_box');
+        let tocBox = $("#TableOfContents");
+        // contentBox.css('height','0')
+            console.log("contentBox",contentBox)
+            // contentBox.css({'height':'0','width':'55px'})    
+            // contentBox.toggleClass("default")
+            if(contentBox.height() === 0){
+            
+            contentBox.css({'height':`${tocBox.height()}px`,'width':'190px'})
+        }else{
+            contentBox.css({'height':'0','width':'7px'})
+        }
+    })
+    
 }
 
 window.addEventListener("scroll", handleScroll);
+
+
+
+    // $('.ext_toc_btn').click(function(){
+    //     let that = this
+    //     console.log($(that));
+    // })
